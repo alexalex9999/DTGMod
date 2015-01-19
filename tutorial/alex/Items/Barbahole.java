324f8f195f1de48d8dc0d8aeec72dd6483fd76c7
gutza1 <peterushtoma@yahoo.com>
2015-01-19 10:57:21 -0500

    Fixed Barahole

--- tutorial/alex/Items/Barbahole.java
+++ tutorial/alex/Items/Barbahole.java
@@ -3,6 +3,7 @@ package tutorial.alex.Items;
 import java.util.ArrayList;
 import java.util.List;
 
+import tutorial.alex.AlexMainRegistry;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBow;
@@ -11,17 +12,20 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 
-public class Barbahole extends AlchemyItem
- {
+public class Barbahole extends AlchemyItem {
  
-{
-super(AlexItemLoader);
-setUnlocalizedName("barbaHole");
-   setCreativeTab(AlexMainRegistry.tabGodcraft);
-     setTextureName("alexmodid:barbahole");
+	public Barbahole(){
+		super();
+		setUnlocalizedName("barbaHole");
+		setCreativeTab(AlexMainRegistry.tabGodcraft);
+		setTextureName("alexmodid:barbahole");
  }
-public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
-{
-	world.createExplosion(EntityPlayer, (float)i + 0.5F, (float)j + 0.5F, (float)k + 0.5F, 3F);
-}
+	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+	{
+		par2World.playSoundAtEntity(par3EntityPlayer, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+		if (!par2World.isRemote){
+			par2World.createExplosion(par3EntityPlayer, par3EntityPlayer.posX, par3EntityPlayer.posY, par3EntityPlayer.posZ, 3F, bFull3D);
+		}
+		return par1ItemStack;
+	}
 }
